336fbdb5fb71a7120449bec963022a0096dee76a
AlexsanderTG <80927240+AlexsanderTG@users.noreply.github.com>
2024-04-01 18:12:40 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,4 @@
+from teste import criar_conta
+from conta_classe import Conta
+
+
---
+++ teste.py
@@ -0,0 +1,16 @@
+
+
+def criar_conta(numero, titular, saldo, limite, conta=None):
+    conta = {"Numero": numero, "Titular": titular, "Saldo": saldo, "Limite": limite}
+    return conta
+
+def deposita(conta, valor):
+    conta["Saldo"] += valor
+
+
+def saca(conta, valor):
+   conta["Saldo"]-= valor
+
+def extrato(conta):
+    print("Saldo é {}".format(conta["Saldo"]))
+
